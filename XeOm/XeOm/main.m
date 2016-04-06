38b1aff9aed0d000d82454836c9a0c15e3d0f821
Tran Quang son <sontq6@sontqs-imac.local>
2016-04-06 15:30:59 +0700

    Tạo source

---
+++ XeOm/XeOm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XeOm
+//
+//  Created by Tran Quang son on 4/6/16.
+//  Copyright © 2016 freelancer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
